99513dcfa3f3976b213168e1796cc0f8e04d03dc
Ella Fox <tinyfluffs@tutanota.com>
2022-10-09 09:30:56 +0200

    Initial commit

---
+++ src/main/java/se/valueguard/kanboard/KanboardApplication.java
@@ -0,0 +1,13 @@
+package se.valueguard.kanboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KanboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KanboardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/se/valueguard/kanboard/KanboardApplicationTests.java
@@ -0,0 +1,13 @@
+package se.valueguard.kanboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KanboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
